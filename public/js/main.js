ae1abe473ba99e078ffcd0c22eac502f516d86e1
joviviz <vieira251002@gmail.com>
2024-11-19 19:41:58 -0300

    metodo de rotas funcionando; Aplicar pagina home em index.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,23 @@
-const app = angular.module("GuitarApp", []);
+const app = angular.module("guitarApp", ['ngRoute']);
+
+angular.module('guitarApp').config(['$routeProvider', 
+        function config($routeProvider){
+            $routeProvider.
+            when('/home', {
+                templateUrl: '/public/views/home.html'
+            }).
+            when('/history', {
+                templateUrl: '/public/views/history.html'
+            }).
+            when('/plans', {
+                templateUrl: '/public/views/plans.html'
+            }).
+            when('/teachers', {
+                templateUrl: '/public/views/teachers.html'
+            }).
+            when('/contact', {
+                templateUrl: '/public/views/contact.html'
+            })
+        }
+    ]);
 
