669b076a4cac3957f8c63b5e61c48dc4ce56c92d
Joviviz <101208535+Joviviz@users.noreply.github.com>
2025-06-12 00:01:38 -0300

    Change main.js routes to fix deploy

--- public/js/main.js
+++ public/js/main.js
@@ -4,25 +4,25 @@ angular.module('guitarApp').config(['$routeProvider',
         function config($routeProvider){
             $routeProvider.
             when('/home', {
-                templateUrl: '/public/views/home.html'
+                templateUrl: 'public/views/home.html'
             }).
             when('/history', {
-                templateUrl: '/public/views/history.html'
+                templateUrl: 'public/views/history.html'
             }).
             when('/plans', {
-                templateUrl: '/public/views/plans.html'
+                templateUrl: 'public/views/plans.html'
             }).
             when('/teachers', {
-                templateUrl: '/public/views/teachers.html'
+                templateUrl: 'public/views/teachers.html'
             }).
             when('/contact', {
-                templateUrl: '/public/views/contact.html'
+                templateUrl: 'public/views/contact.html'
             }).
             when('/lore', {
-                templateUrl: '/public/views/lore.html'
+                templateUrl: 'public/views/lore.html'
             }).
             when('/luigi_sings', {
-                templateUrl: '/public/views/luigi_sings.html'
+                templateUrl: 'public/views/luigi_sings.html'
             }).
             otherwise('/home')
         }
